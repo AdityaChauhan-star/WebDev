d7d98a53a5323613f2ea4c04d100e2671902e331
Aditya Chauhan <officialemail032@gmail.com>
2025-04-11 01:05:06 +0530

    else if

--- Javascript/app.js
+++ Javascript/app.js
@@ -1,12 +1,22 @@
-let color = "green";
+let color = "red";
 
 // Traffic Light Simulation
 if (color === "red") {
     console.log("Stop!");
-}
-if (color === "yellow") {
+} else if (color === "yellow") {
     console.log("Get ready to stop!");
-}
-if (color === "green") {
+} else if (color === "green") {
     console.log("Go!");
+}
+
+let marks = 33;
+
+if (marks >= 90){
+    console.log("A+");
+} else if (marks >= 60) {
+    console.log("B");
+} else if (marks >= 40) {
+    console.log("C");
+} else if (marks <= 33) {
+    console.log("F");
 }
