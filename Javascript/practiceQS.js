28c16ddf7b64d864012b9359c8156fa758142d5b
Aditya Chauhan <officialemail032@gmail.com>
2025-04-11 18:43:06 +0530

    else

---
+++ Javascript/practiceQS.js
@@ -0,0 +1,11 @@
+let size = "S";
+
+if (size === "XL") {
+    console.log('Price is RS.250');
+} else if (size === "L") {
+    console.log('Price is RS.200');
+} else if (size === "M") {
+    console.log('Price is RS.100');
+} else {
+    console.log("Price is RS.50")
+}
