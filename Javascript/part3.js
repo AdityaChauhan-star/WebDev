3c2d377709c30f8e9a4595a12a1183256426489b
Aditya Chauhan <officialemail032@gmail.com>
2025-04-14 02:12:03 +0530

    part3

---
+++ Javascript/part3.js
@@ -0,0 +1,8 @@
+let students = ["Aditya", "Amit", "Ankit", "Anshika", "Arjun", "Aarav", "Aditi", "Aanya", "Aarvi", "Aarush"];
+let marks = [90, 85, 78, 88, 92, 95, 80, 75, 89, 91];
+let info = ["aditya", 25, 6.1];  //MIXED DATA TYPE
+let newArr = [];  //Empty array
+
+let fruits = ["Apple", "Banana", "Cherry", "Date", "Elderberry", "Fig", "Grape"];
+
+let cars = ["BMW", "Audi", "Mercedes", "Toyota", "Honda", "Ford", "Chevrolet"];
