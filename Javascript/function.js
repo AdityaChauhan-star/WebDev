03964cfdf265bbe423f0f105ffc0df258d4a096e
Aditya Chauhan <officialemail032@gmail.com>
2025-07-20 23:08:05 +0530

    Comment out unused functions and interval logic in function.js

--- Javascript/function.js
+++ Javascript/function.js
@@ -1,15 +1,30 @@
-let sum = function(a, b) {
-    return a + b;
-}
+// let sum = function(a, b) {
+//     return a + b;
+// }
 
-let hello = function () {
-    console.log('Hello');
-}
+// let hello = function () {
+//     console.log('Hello');
+// }
 
-let id = setInterval( () => {
-    console.log("hello Aditya")
-}, 4000);
+// let id = setInterval( () => {
+//     console.log("hello Aditya")
+// }, 4000);
 
-setTimeout( () => {
-    clearInterval(id);
-},10000);
+// setTimeout( () => {
+//     clearInterval(id);
+// },10000);
+
+//Destructuring
+
+const student = {
+    name : "Karan",
+    age : 14,
+    class : 9,
+    username : "Karan@.com",
+    password : "1234"
+};
+
+// let username = student.username;
+// let password = student.password;
+
+let {username , password} = student;
